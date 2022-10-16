dcbda5f8c5b68a550c7a9da8d7a0a22aa9ed5508
Nicholas Noronha <nicholascaio@hotmail.com>
2022-10-16 18:33:47 -0300

    fix: add api port to env variables and uninstall bodyparser

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const router = require('./app/routes/usersRoutes')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./app/routes/usersRoutes");
+require("dotenv").config();
 
+const port = process.env.API_PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
-app.listen(3000, () => console.log('Server is running'))
+app.listen(port, () => console.log("Server is running"));
